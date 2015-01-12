f873954a336870227cc05986268d2d9879245cf6
kleong153@gmail.com <kleong153@gmail.com@eb7bba31-5b47-53b1-aa80-5c3af2112fcf>
2015-01-12 09:02:37 +0000

    VAC testing.

--- apps/frontend/modules/home/actions/actions.class.php
+++ apps/frontend/modules/home/actions/actions.class.php
@@ -30,7 +30,6 @@ class homeActions extends sfActions
   {
     $username = $this->getRequestParameter("username");
     $password = $this->getRequestParameter("password");
-
     $success = 0;
 
     if ($username == "aio" && $password == "123") {
@@ -43,5 +42,30 @@ class homeActions extends sfActions
 
     echo json_encode($arr);
     return sfView::HEADER_ONLY;
+  }
+
+  public function executeVacCreate()
+  {
+    $username = $this->getRequestParameter("username");
+    $code = $this->getRequestParameter("code");
+    $vehicle_no = $this->getRequestParameter("vehicle_no");
+    $success = 0;
+    $datetime = new DateTime();
+
+    $arr = array(
+        "success" => $success,
+        "created_on" => $datetime->format("n/j/Y G:i"), // M/D/Y H:mm
+        "expired_on" => 60 * 2 // In minutes, 2 hours
+    );
+
+    echo json_encode($arr);
+    return sfView::HEADER_ONLY;
+  }
+
+  public function executeVacVerifyTac()
+  {
+    $username = $this->getRequestParameter("username");
+    $tac = $this->getRequestParameter("tac");
+
   }
 }
